24f1a0c076b8ce9b6b3ca4b4763523d96c66d23d
erosiim <exkapp@gmail.com>
2025-10-07 21:15:23 -0600

    agrega comentarios a interfaces funcionales

--- src/main/java/org/eros/java_practicing/java11/INotificador.java
+++ src/main/java/org/eros/java_practicing/java11/INotificador.java
@@ -1,5 +1,10 @@
 package org.eros.java_practicing.java11;
-
+/*
+Las interfaces funcionales pueden ser implementadas por
+otras clases como normalmente se hace,
+pero el plus de este tipo de interfaces es delegar la implementación con lambdas
+al momento que es requerida y no tener implementaciones fijas
+ */
 @FunctionalInterface
 public interface INotificador {
 
